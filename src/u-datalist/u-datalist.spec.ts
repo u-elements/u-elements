6c5629b47819df198b5638aea27260edd82f4c1c
eirikbacker <eirik.backer@gmail.com>
2024-01-18 21:57:21 +0100

    fix: add multiple u-datalist test

--- src/u-datalist/u-datalist.spec.ts
+++ src/u-datalist/u-datalist.spec.ts
@@ -218,130 +218,28 @@ describe('u-datalist', () => {
     expect(uDatalist.hidden).toBe(false)
   })
 
-  /*test('generates hidden native summary element', () => {
-    const uDetails = toDOM<UHTMLDetailsElement>(`
-      <u-details>
-        <u-summary>Summary 1</u-summary>
-        <details>Details 1</details>
-      </u-details>
+  test('handles multiple u-datalist on same page', async () => {
+    const div = toDOM(`
+      <div>
+        ${DEFAULT_TEST_HTML}
+        ${DEFAULT_TEST_HTML.replace(/datalist-1/g, 'datalist-2')}
+      </div>
     `)
-    const nativeDetails = uDetails.lastElementChild as HTMLDetailsElement
-    const nativeSummary = nativeDetails.firstElementChild as HTMLElement
+    const items1 = [...div.querySelectorAll('form:first-child *')]
+    const items2 = [...div.querySelectorAll('form:last-child *')]
+    const [, input1, uDatalist1] = items1 as [HTMLLabelElement, HTMLInputElement, UHTMLDataListElement];
+    const [, input2, uDatalist2] = items2 as [HTMLLabelElement, HTMLInputElement, UHTMLDataListElement];
 
-    expect(nativeSummary).toBeInstanceOf(HTMLElement)
-    expect(nativeSummary.nodeName).toBe('SUMMARY')
-    expect(nativeSummary.hidden).toBe(true)
+    input1.focus()
+    await vi.waitFor(() => {
+      expect(uDatalist1.hidden).toBe(false)
+      expect(uDatalist2.hidden).toBe(true)
+    }, NEXT_TICK)
+
+    input2.focus()
+    await vi.waitFor(() => {
+      expect(uDatalist2.hidden).toBe(false)
+      expect(uDatalist1.hidden).toBe(true)
+    }, NEXT_TICK)
   })
-
-  test('sets up attributes when native details is appended', async () => {
-    const uDetails = toDOM<UHTMLDetailsElement>(`<u-details><u-summary>Summary 1</u-summary></u-details>`)
-    uDetails.insertAdjacentHTML('beforeend', '<details>Details 1</details>')
-
-    const [uSummary, nativeDetails] = [...uDetails.children] as [UHTMLSummaryElement, HTMLDetailsElement]
-    await vi.waitFor(() => expect(nativeDetails.id).toBe(uSummary.getAttribute('aria-controls')), NEXT_TICK)
-  })
-
-  test('handles up open property and attributes change', () => {
-    const uDetails = toDOM<UHTMLDetailsElement>(`
-      <u-details>
-        <u-summary>Summary 1</u-summary>
-        <details>Details 1</details>
-      </u-details>
-    `)
-    const [uSummary, nativeDetails] = [...uDetails.children] as [UHTMLSummaryElement, HTMLDetailsElement]
-
-    expect(uSummary.role).toBe('button')
-    expect(uSummary.id).toBe(nativeDetails.getAttribute('aria-labelledby'))
-    expect(uSummary.tabIndex).toBe(0)
-    expect(uSummary.getAttribute('aria-expanded')).toBe('false')
-    expect(uSummary.getAttribute('aria-controls')).toBe(nativeDetails.id)
-    expect(nativeDetails.role).toBe('group')
-    expect(nativeDetails.getAttribute('aria-hidden')).toBe('true')
-  })
-
-  test('handles open property and attributes change', () => {
-    const uDetails = toDOM<UHTMLDetailsElement>(`
-      <u-details>
-        <u-summary>Summary 1</u-summary>
-        <details>Details 1</details>
-      </u-details>
-    `)
-    const [uSummary, nativeDetails] = [...uDetails.children] as [UHTMLSummaryElement, HTMLDetailsElement]
-
-    uDetails.open = true
-    expect(uDetails.hasAttribute('open')).toBe(true)
-    expect(uSummary.getAttribute('aria-expanded')).toBe('true')
-    expect(nativeDetails.getAttribute('aria-hidden')).toBe('false')
-    expect(nativeDetails.hasAttribute('open')).toBe(true)
-
-    uDetails.removeAttribute('open')
-    expect(uDetails.open).toBe(false)
-    expect(uSummary.getAttribute('aria-expanded')).toBe('false')
-    expect(nativeDetails.getAttribute('aria-hidden')).toBe('true')
-    expect(nativeDetails.hasAttribute('open')).toBe(false)
-
-    uDetails.setAttribute('open', 'banana')
-    expect(uDetails.open).toBe(true)
-    expect(uSummary.getAttribute('aria-expanded')).toBe('true')
-    expect(nativeDetails.getAttribute('aria-hidden')).toBe('false')
-    expect(nativeDetails.hasAttribute('open')).toBe(true)
-
-    uDetails.open = false
-    expect(uDetails.open).toBe(false)
-    expect(uSummary.getAttribute('aria-expanded')).toBe('false')
-    expect(nativeDetails.getAttribute('aria-hidden')).toBe('true')
-    expect(nativeDetails.hasAttribute('open')).toBe(false)
-  })
-
-  test('respects id attributes', async () => {
-    const uDetails = toDOM<UHTMLDetailsElement>(`
-      <u-details>
-        <u-summary id="summary-1">Summary 1</u-summary>
-        <details id="details-1">Details 1</details>
-      </u-details>
-    `)
-    const [uSummary, nativeDetails] = [...uDetails.children] as [UHTMLSummaryElement, HTMLDetailsElement]
-
-    expect(uSummary.id).toBe('summary-1')
-    expect(nativeDetails.id).toBe('details-1')
-    expect(uSummary.getAttribute('aria-controls')).toBe('details-1')
-    expect(nativeDetails.getAttribute('aria-labelledby')).toBe('summary-1')
-
-    uSummary.id = 'summary-1-changed-id'
-    await vi.waitFor(() => expect(nativeDetails.getAttribute('aria-labelledby')).toBe('summary-1-changed-id'), NEXT_TICK)
-    
-    nativeDetails.id = 'details-1-changed-id'
-    await vi.waitFor(() => expect(uSummary.getAttribute('aria-controls')).toBe('details-1-changed-id'), NEXT_TICK)
-  })
-
-  test('updates attributes on click', () => {
-    const uDetails = toDOM<UHTMLDetailsElement>(`
-      <u-details>
-        <u-summary>Summary 1</u-summary>
-        <details>Details 1</details>
-      </u-details>
-    `)
-    const [uSummary] = [...uDetails.children] as [UHTMLSummaryElement]
-
-    uSummary.click()
-    expect(uDetails.open).toBe(true)
-
-    uSummary.focus()
-    document.activeElement?.dispatchEvent(new KeyboardEvent("keydown", { key: ' ', bubbles: true }))
-    expect(uDetails.open).toBe(false)   
-  })
-
-  test('respects nativeDetails open', async () => {
-    const uDetails = toDOM<UHTMLDetailsElement>(`
-      <u-details>
-        <u-summary>Summary 1</u-summary>
-        <details>Details 1</details>
-      </u-details>
-    `)
-    const nativeDetails = uDetails.lastElementChild as HTMLDetailsElement
-
-    expect(uDetails.open).toBe(false)
-    ;(nativeDetails.firstElementChild as HTMLElement).click()
-    await vi.waitFor(() => expect(uDetails.open).toBe(true), NEXT_TICK)
-  })*/
 })

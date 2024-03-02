ad4a9037fe61fb47acd28d82319c0cfbcb8fc832
eirikbacker <eirik.backer@gmail.com>
2024-03-02 10:18:47 +0100

    fix: use vue-codemirror6

--- docs/.vitepress/components/sandbox.vue
+++ docs/.vitepress/components/sandbox.vue
@@ -1,14 +1,14 @@
 <script setup>
+  import CodeMirror from 'vue-codemirror6';
   import { ref, watch } from 'vue'
-  import { Codemirror } from 'vue-codemirror'
   import { html } from '@codemirror/lang-html'
-
+  
   // Import all uElements
   const modules = Object.values(import.meta.glob('../../../packages/*/u-!(*.spec).ts'))
   const loading = Promise.all(modules.map((module) => module()))
-  const htmlConfig = ref(html());
+  const htmlLang = ref(html());
   
-  // Auto generate htmlConfig for codeMirror
+  // Auto generate htmlLang for codeMirror
   loading.then((klasses) => {
     const extraTags = Object.fromEntries(
       klasses
@@ -18,29 +18,31 @@
           { globalAttrs: true, attrs: Object.fromEntries(attr.map((v) => [v, null])) }
         ])
       )
-    htmlConfig.value = html({ extraTags });
+      htmlLang.value = html({ extraTags });
   })
 
   let timer = null
   const code = ref('')
   const view = ref(null)
   const slots = ref(null)
-  const style = { height: '100%', outline: 'none!important', fontSize: '.875em' }
-  const updateView = () => (view.value.innerHTML = code.value);
-
+  const updateView = () => view.value && (view.value.innerHTML = code.value);
+  
   watch(slots, () => updateView(code.value = slots.value.textContent.trim()))
   watch(code, () => clearTimeout(timer) || (timer = setTimeout(updateView, 300)))
 </script>
 <style>
   .demo { border-radius: 8px; border: 2px dashed var(--vp-c-divider); display: flex; margin-block: .5em; overflow: hidden }
-  .demo > div { box-sizing: border-box; display: block!important; width: 50% }
-  .demo > div:last-child { border-left: inherit; border-width: 0 0 0 2px; min-height: 200px; padding: 1rem }
-  .demo > div:last-child :is(button,input) { all: revert }
+  .demo-code, .demo-view { box-sizing: border-box; width: 50% }
+  .demo-code > * { font-size: .875rem; height: 100%; outline: none!important }
+  .demo-view { border-left: inherit; border-width: 0 0 0 2px; min-height: 200px; padding: 1rem }
+  .demo-view :is(button,input) { all: revert }
 </style>
 <template>
   <pre ref="slots" hidden><slot></slot></pre>
   <div class="demo">
-    <codemirror :extensions="[htmlConfig]" :style="style" :tab-size="2" v-model="code" />
-    <div ref="view"></div>
+    <ClientOnly>
+      <CodeMirror class="demo-code" basic :lang="htmlLang" v-model="code" />
+    </ClientOnly>
+    <div class="demo-view" ref="view"></div>
   </div>
 </template>

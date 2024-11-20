64700e61d9aa8b43d03f2792824c7a0c2b0b98be
Morten Kjelling <Morten.kjelling@nrk.no>
2024-11-20 10:22:08 +0100

    reactive variable controlled u-tabs

--- frameworks/svelte/components/Tabs.svelte
+++ frameworks/svelte/components/Tabs.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import "../../../packages/u-tabs/dist/u-tabs";
   import selectedTabStore, { tabs } from "./store.js";
+  const selected = $selectedTabStore.name;
+  let showtada = false;
 </script>
 
 <u-tabs>
@@ -9,17 +11,42 @@
       <!-- svelte-ignore a11y-click-events-have-key-events -->
       <!-- svelte-ignore a11y-no-static-element-interactions -->
       <u-tab
-        aria-selected={$selectedTabStore.name === tab.name}
+        aria-selected={selected === tab.name}
         on:click={() => selectedTabStore.set(tab)}
         aria-controls={`tab-${tab.name}`}
       >
-        {tab.name}
+        {tab.name} - {selected === tab.name}
       </u-tab>
     {/each}
   </u-tablist>
   <u-tabpanel id={`tab-${tabs[0].name}`}> Panel 1 with ✨ </u-tabpanel>
   <u-tabpanel id={`tab-${tabs[1].name}`}> Panel 2 with 🎖️ </u-tabpanel>
   <u-tabpanel id={`tab-${tabs[2].name}`}> Panel 3 with ¿ </u-tabpanel>
+</u-tabs>
+
+<u-tabs>
+  <u-tablist>
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <!-- svelte-ignore a11y-no-static-element-interactions -->
+    <u-tab
+      aria-selected={showtada}
+      on:click={() => (showtada = true)}
+      aria-controls={`tab-tada`}
+    >
+      tada
+    </u-tab>
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <!-- svelte-ignore a11y-no-static-element-interactions -->
+    <u-tab
+      aria-selected={!showtada}
+      on:click={() => (showtada = false)}
+      aria-controls={`tab-meh`}
+    >
+      meh
+    </u-tab>
+  </u-tablist>
+  <u-tabpanel id={`tab-tada`}> Panel tada 🎉 </u-tabpanel>
+  <u-tabpanel id={`tab-meh`}> Panel meh 😥 </u-tabpanel>
 </u-tabs>
 
 <style>

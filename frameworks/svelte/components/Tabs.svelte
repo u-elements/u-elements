2a8d413c70bd8858e484863d96ef762d52154597
Morten Kjelling <Morten.kjelling@nrk.no>
2024-11-20 09:54:45 +0100

    localstorage backed tabs

--- frameworks/svelte/components/Tabs.svelte
+++ frameworks/svelte/components/Tabs.svelte
@@ -1,16 +1,25 @@
 <script lang="ts">
-  import '../../../packages/u-tabs/dist/u-tabs'
+  import "../../../packages/u-tabs/dist/u-tabs";
+  import selectedTabStore, { tabs } from "./store.js";
 </script>
 
 <u-tabs>
   <u-tablist>
-    <u-tab>Tab 1</u-tab>
-    <u-tab aria-selected="true">Tab 2</u-tab>
-    <u-tab>Tab 3</u-tab>
+    {#each tabs as tab (tab.name)}
+      <!-- svelte-ignore a11y-click-events-have-key-events -->
+      <!-- svelte-ignore a11y-no-static-element-interactions -->
+      <u-tab
+        aria-selected={$selectedTabStore.name === tab.name}
+        on:click={() => selectedTabStore.set(tab)}
+        aria-controls={`tab-${tab.name}`}
+      >
+        {tab.name}
+      </u-tab>
+    {/each}
   </u-tablist>
-  <u-tabpanel>Panel 1 with <a href="#">link</a></u-tabpanel>
-  <u-tabpanel>Panel 2 with <a href="#">link</a></u-tabpanel>
-  <u-tabpanel>Panel 3 with <a href="#">link</a></u-tabpanel>
+  <u-tabpanel id={`tab-${tabs[0].name}`}> Panel 1 with ✨ </u-tabpanel>
+  <u-tabpanel id={`tab-${tabs[1].name}`}> Panel 2 with 🎖️ </u-tabpanel>
+  <u-tabpanel id={`tab-${tabs[2].name}`}> Panel 3 with ¿ </u-tabpanel>
 </u-tabs>
 
 <style>
@@ -18,12 +27,12 @@
     padding: 0.5em;
   }
 
-  u-tab[aria-selected='true'] {
+  u-tab[aria-selected="true"] {
     border-bottom: 4px solid;
   }
 
   u-tabpanel {
-    padding: 1em;
+    padding: 3em;
     border: 1px solid;
   }
 </style>

48b8aae4710b96133b2b89593d593604f5b44fa8
eirikbacker <eirik.backer@mattilsynet.no>
2025-03-05 11:21:12 +0100

    fix(Svelte): expose both old and new event attribute support

--- frameworks/svelte/App.svelte
+++ frameworks/svelte/App.svelte
@@ -1,24 +1,26 @@
 <script lang="ts">
-  import './../../packages/u-progress'
-  import './../../packages/u-datalist'
-  import './../../packages/u-tags'
-  import Tabs from './Tabs.svelte'
+  import '../../packages/u-datalist'
+  import '../../packages/u-tags'
+  // import Tabs from './Tabs.svelte'
+
+  function handleTags(event: CustomEvent) {
+    console.log(event.detail)
+  }
 </script>
 
 <main>
   <h1>Svelte + u-elements</h1>
   <u-progress value="5" max="10">33%</u-progress>
   <br />
-  <u-datalist id="my-list" data-sr-singular="%d hit" data-sr-plural="%d hits">
-    <u-option value="test-1">Test 1</u-option>
-    <u-option value="test-2">Test 2</u-option>
-    <u-option value="test-3">Test 3</u-option>
-  </u-datalist>
-  <u-tags on:tags={(event) => console.log(event.detail)}>
+  <u-tags ontags={handleTags}>
     <data>Kokkos</data>
     <data>Banan</data>
     <data>Jordbær</data>
-    <input type="text" />
+    <input type="text" list="my-list" />
+    <u-datalist id="my-list" data-sr-singular="%d hit" data-sr-plural="%d hits">
+      <u-option value="test-1">Test 1</u-option>
+      <u-option value="test-2">Test 2</u-option>
+      <u-option value="test-3">Test 3</u-option>
+    </u-datalist>
   </u-tags>
-  <Tabs />
 </main>
--- tsup.config.ts
+++ tsup.config.ts
@@ -76,7 +76,7 @@ export type Preact${type} = ${
 			isNative
 				? `PreactTypes.JSX.IntrinsicElements['${tagNative}']`
 				: `PreactTypes.JSX.HTMLAttributes<UHTMLTagsElement> & { ${events
-						.map(([, type, event]) => `on${type}: (event: ${event}) => void`)
+						.map(([, type, event]) => `on${type}?: (event: ${event}) => void`)
 						.join("; ")} }`
 		}
 export type React${type} = ${
@@ -90,7 +90,7 @@ export type Svelte${type} = ${
 			isNative
 				? `SvelteTypes.SvelteHTMLElements['${tagNative}']`
 				: `SvelteTypes.HTMLAttributes<${domInterface}> & { ${events
-						.map(([, type, event]) => `'on:${type}': (event: ${event}) => void`)
+						.map(([, type, event]) => `'on:${type}'?: (event: ${event}) => void, on${type}?: (event: ${event}) => void`)
 						.join("; ")} }`
 		}
 export type Solid${type} = ${

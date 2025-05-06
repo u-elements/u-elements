d43829b925f698bd6710235d341d07d23a139e69
eirikbacker <eirik.backer@mattilsynet.no>
2025-05-06 14:29:26 +0200

    chore: update dependencies

--- frameworks/svelte/index.ts
+++ frameworks/svelte/index.ts
@@ -1,7 +1,6 @@
+import { mount } from 'svelte';
 import App from './App.svelte'
 
-const app = new App({
-  target: document.getElementById('app') as HTMLElement,
+export default mount(App, {
+  target: document.getElementById('app')!,
 })
-
-export default app
